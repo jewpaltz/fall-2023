8a23b3ac9c5eb7871cdc09d21d5044569774265e
Moshe Plotkin <mplotkin@jewpaltz.com>
2023-11-09 14:38:15 -0500

    installed toasts

--- client/src/model/session.ts
+++ client/src/model/session.ts
@@ -2,20 +2,34 @@
 */
 import { reactive } from "vue";
 import { useRouter } from "vue-router"
+import { useToast } from "vue-toastification";
 import * as myFetch from "./myFetch";
 import { type User, getUserByEmail } from "./users";
+
+const toast = useToast();
 
 const session = reactive({
   user: null as User | null,
   redirectUrl: null as string | null,
+  messages: [] as {
+    type: string,
+    text: string
+  }[]
 })
 
 export function api(action: string){
+  showError("api() is deprecated. Use myFetch.api() instead.");
   return myFetch.api(`${action}`)
 }
 
 export function getSession(){
   return session;
+}
+
+export function showError(err: any){
+  console.error(err);
+  session.messages.push({ type: "error", text: err.message ?? err});
+  toast.error( err.message ?? err);
 }
 
 export function useLogin(){
