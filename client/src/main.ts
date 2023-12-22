dab09fbaf7909633bf2ef423a9fcb62506c3d38e
Moshe Plotkin <mplotkin@jewpaltz.com>
2023-12-21 22:32:57 -0500

    added oruga to the project

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,16 @@
 import './assets/main.css'
+import '@oruga-ui/theme-oruga/dist/scss/oruga-full.scss'
 import "vue-toastification/dist/index.css";
 
 import { createApp } from 'vue'
 import Toast from "vue-toastification";
+import Oruga from '@oruga-ui/oruga-next';
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router).use(Toast, {})
+app.use(router).use(Toast, {}).use(Oruga)
 
 app.mount('#app')
